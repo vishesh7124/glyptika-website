032fcb7dc845c6a0f96d7f47602959555b5cd91f
vishesh7124 <87852777+vishesh7124@users.noreply.github.com>
2024-11-21 19:11:47 +0530

    about 3d

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -11,7 +11,7 @@ export const Experience = ()=>{
     let tex = useTexture(earthTexture)
     const boxMaterial = new THREE.MeshStandardMaterial({color:'white'})
     const sphereMaterial = new THREE.MeshStandardMaterial({color:'white',map:tex})
-    const items = ["box", "sphere"];
+    const items = ["box", "sphere","plane"];
     const [currentIndex, setCurrentIndex] = useState(0);
     const [visibleItem, setVisibleItem] = useState("box");
     const [mobile, setMobile] = useState(false);
@@ -87,7 +87,7 @@ export const Experience = ()=>{
             </mesh>
         )}
         
-        {/* {(visibleItem === "plane") && (
+        {(visibleItem === "plane") && (
             <Model
                 rotation={[1.4, 0.6, -5.7]}
                 scale={0.5}
@@ -96,7 +96,7 @@ export const Experience = ()=>{
                 dissolveVisible={itemDisplayed === "plane"}
                 onFadeOut={onFadeOut}
             />
-        )} */}
+        )}
         {/* {(visibleItem === "cloud") && (
             <Cloud
                 // rotation={[1.4, 0.6, -5.7]}
