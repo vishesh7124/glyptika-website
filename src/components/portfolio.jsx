e5153e05becec9baa795ab60e19ab6a56a08a1d9
vishesh7124 <87852777+vishesh7124@users.noreply.github.com>
2024-11-28 17:23:36 +0530

    added fallback

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -2,27 +2,31 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { PerformanceMonitor } from "@react-three/drei";
 import Cyl from "./Cyl";
-// import { EffectComposer,Bloom,ToneMapping } from '@react-three/postprocessing'
+import { EffectComposer,Bloom,ToneMapping } from '@react-three/postprocessing'
 import { useState } from "react";
 
 
 
 const Portfolio = ()=>{
     const [dpr, setDpr] = useState(1)   
+    const [visible,setVisible] = useState(true)
+    const optimizePerformance = ()=>{
+        setVisible(false);
+    }
     return(
         <div className="section portfolio" id="portfolio">
             {/* <h1 className="text-head-sub text-uppercase">Portfolio</h1> */}
             <Canvas flat camera={{fov:22}} frameloop="demand" className="cylinder" dpr={1}>
-                <PerformanceMonitor flipflops={3} onFallback={() => console.log("fallback")}>
+                <PerformanceMonitor flipflops={3} onFallback={optimizePerformance}>
 
 
                 {/* <OrbitControls/> */}
-                <ambientLight intensity={2}/>
+                <ambientLight intensity={4}/>
                 <Cyl/>
-                {/* <EffectComposer>
+{                visible && <EffectComposer>
                     <Bloom
-                        mipmapBlur
-                        intensity={0} // The bloom intensity.
+                        // mipmapBlur
+                        intensity={1} // The bloom intensity.
                         // blurPass={undefined} // A blur pass.
                         // kernelSize={KernelSize.LARGE} // blur kernel size
                         luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
@@ -30,9 +34,9 @@ const Portfolio = ()=>{
                         // mipmapBlur={false} // Enables or disables mipmap blur.
                         // resolutionX={Resolution.AUTO_SIZE} // The horizontal resolution.
                         // resolutionY={Resolution.AUTO_SIZE} // The vertical resolution.
-                    />x
+                    />
                     <ToneMapping adaptive/>
-                </EffectComposer> */}
+                </EffectComposer>}
                 </PerformanceMonitor>
             </Canvas>
             <div className="portfolio-head ">
